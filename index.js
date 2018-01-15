7a8f50beefe99d3c44332b2f95021e7f9745e149
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-15 20:06:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const token = 'd5a37406e38cc71a1707bc1199f0aa075f938bb7'
-fetch('https://api.github.com/user/repos', {
+fetch('https://api.github.com/dkStephanos/repos', {
   headers: {
     Authorization: `token ${token}`
   }

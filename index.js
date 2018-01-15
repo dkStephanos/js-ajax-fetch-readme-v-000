9ef64c7c685ed317250e509b897d0ef80fa45f4c
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-15 20:04:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const app = "I don't do much.";
-
-fetch('https://api.github.com/user/repos').
-  then(res => res.json()).
-  then(json => console.log(json))
+const token = 'd5a37406e38cc71a1707bc1199f0aa075f938bb7'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
